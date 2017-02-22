daec16055c36321823e094d7725817d223f3439b
dhru95 <dhruroy95@gmail.com>
2017-02-22 01:09:05 -0500

    final

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Bare - Start Bootstrap Template (for IS219)</title>
+    <title>Bare - Dhrubo Roy </title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
